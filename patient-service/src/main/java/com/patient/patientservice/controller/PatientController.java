3cf3b8d932055356f3f6488b1076aa271938da17
Hasanga Ranasinghe <dinithhasangare@gmail.com>
2025-11-25 12:08:04 +0530

    Add PatientController class for handling patient-related requests

---
+++ patient-service/src/main/java/com/patient/patientservice/controller/PatientController.java
@@ -0,0 +1,27 @@
+package com.patient.patientservice.controller;
+
+import com.patient.patientservice.dto.PatientResponseDTO;
+import com.patient.patientservice.service.PatientService;
+import java.util.List;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/patients")
+public class PatientController {
+
+  private final PatientService patientService;
+
+  public PatientController(PatientService patientService) {
+    this.patientService = patientService;
+  }
+
+  @GetMapping
+  public ResponseEntity<List<PatientResponseDTO>> getPatients() {
+    List<PatientResponseDTO> patients = patientService.getPatients();
+    return ResponseEntity.ok().body(patients);
+  }
+
+}
