3e94043615fec8cdc621536e6e7dd927bab09051
Hasanga Ranasinghe <dinithhasangare@gmail.com>
2025-11-25 14:29:16 +0530

    Add createPatient endpoint to PatientController for patient creation

--- patient-service/src/main/java/com/patient/patientservice/controller/PatientController.java
+++ patient-service/src/main/java/com/patient/patientservice/controller/PatientController.java
@@ -1,10 +1,16 @@
 package com.patient.patientservice.controller;
 
 import com.patient.patientservice.dto.PatientResponseDTO;
+import com.patient.patientservice.dto.PatientRequestDTO;
 import com.patient.patientservice.service.PatientService;
+
+import jakarta.validation.Valid;
+
 import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +28,12 @@ public class PatientController {
   public ResponseEntity<List<PatientResponseDTO>> getPatients() {
     List<PatientResponseDTO> patients = patientService.getPatients();
     return ResponseEntity.ok().body(patients);
+  }
+
+  @PostMapping
+  public ResponseEntity<PatientResponseDTO> createPatient(@Valid @RequestBody PatientRequestDTO patientRequestDTO) {
+    PatientResponseDTO createdPatient = patientService.createPatient(patientRequestDTO);
+    return ResponseEntity.status(201).body(createdPatient);
   }
 
 }
