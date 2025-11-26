a02de1ab8120178ab89a5086f851ccaf3f8781b9
Hasanga Ranasinghe <dinithhasangare@gmail.com>
2025-11-26 18:07:01 +0530

    Initialize billing service with Maven setup; add application properties and main application class

---
+++ billing-service/src/test/java/com/patient/billing_service/BillingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patient.billing_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
