f4cd42db3104e49f6a8e610028037e755bc0ad75
Hasanga Ranasinghe <dinithhasangare@gmail.com>
2025-11-29 15:50:55 +0530

    test: configure SpringBootTest with JWT secret for AuthService tests

--- auth-service/src/test/java/com/patient/auth_service/AuthServiceApplicationTests.java
+++ auth-service/src/test/java/com/patient/auth_service/AuthServiceApplicationTests.java
@@ -3,7 +3,7 @@ package com.patient.auth_service;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {"jwt.secret=V3l2R1hKMGZTRWR5T1JsaFZ5b21vNEdjZ0Z2ajdwQ0k="})
 class AuthServiceApplicationTests {
 
 	@Test
